9be19143617f4c2bb50d678147c2432af4bda819
CheolJe <me@falsy.me>
2024-06-06 19:54:50 +0900

    feat: remove core codes

--- src/core/dtos/interfaces/ITrackerDTO.ts
+++
@@ -1,7 +0,0 @@
-export default interface ITrackerDTO {
-  readonly id: string
-  readonly carrierId: string
-  readonly label: string
-  readonly trackingNumber: string
-  readonly memos: string[]
-}
--- src/core/repositories/interfaces/ICarrierRepository.ts
+++
@@ -1,7 +0,0 @@
-import ICarrier from "../../domains/entities/interfaces/ICarrier"
-import ILayerDTO from "../../dtos/interfaces/ILayerDTO"
-
-export default interface ICarrierRepository {
-  getCarriers?(): Promise<ILayerDTO<ICarrier[]>>
-  getCarrier?(carrierId: string): Promise<ILayerDTO<ICarrier>>
-}
