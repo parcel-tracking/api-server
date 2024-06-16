0ad00e3221216fc4edc93886256633abf8526c18
CheolJe <me@falsy.me>
2024-06-16 17:31:15 +0900

    feat: add extension CORS settings

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { AppModule } from "./frameworks/moduls/AppModule"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const allowedOrigins = [`chrome-extension://${process.env.WH_EXTENSION_ID}`]
+
   app.enableCors({
-    origin: "*"
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true
   })
+
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
