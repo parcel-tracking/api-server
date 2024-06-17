db6676af4f5fdab6245fcb467198bae6b3e72cc7
CheolJe <me@falsy.me>
2024-06-17 11:14:28 +0900

    feat: allow CORS for app review

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,24 @@ import { AppModule } from "./frameworks/moduls/AppModule"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  const allowedOrigins = [
-    `chrome-extension://${process.env.EXTENSION_TEST_ID}`,
-    `chrome-extension://${process.env.EXTENSION_ID}`
-  ]
+  // const allowedOrigins = [
+  //   `chrome-extension://${process.env.EXTENSION_TEST_ID}`,
+  //   `chrome-extension://${process.env.EXTENSION_ID}`
+  // ]
+
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       callback(null, true)
+  //     } else {
+  //       callback(new Error("Not allowed by CORS"))
+  //     }
+  //   },
+  //   credentials: true
+  // })
 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true
+    origin: "*"
   })
 
   await app.listen(process.env.PORT || 3000)
