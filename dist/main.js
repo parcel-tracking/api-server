37597aa304302062f073606a947aa4245e2b767a
CheolJe <me@falsy.me>
2024-08-14 20:37:26 +0900

    feat: allow CORS

--- dist/main.js
+++ dist/main.js
@@ -4,20 +4,8 @@ const core_1 = require("@nestjs/core");
 const AppModule_1 = require("./frameworks/moduls/AppModule");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(AppModule_1.AppModule);
-    const allowedOrigins = [
-        `chrome-extension://${process.env.EXTENSION_TEST_ID}`,
-        `chrome-extension://${process.env.EXTENSION_ID}`
-    ];
     app.enableCors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            }
-            else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true
+        origin: "*"
     });
     await app.listen(process.env.PORT || 3000);
 }
